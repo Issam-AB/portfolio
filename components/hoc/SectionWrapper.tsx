6a8ba1e1ebc7d380bbc85495287441f09f724b34
Issam <Issam.aboulfadl05@gmail.com>
2023-03-15 00:47:22 +0100

    add timeline experience

--- components/hoc/SectionWrapper.tsx
+++ components/hoc/SectionWrapper.tsx
@@ -4,23 +4,23 @@ import { styles } from "../../styles";
 import { staggerContainer } from "../../utils/motion";
 
 type Props = {
-  Component: React.FC;
-  idName: string;
+	Component: React.FC;
+	idName: string;
 };
 
 const SectionWrapper: React.FC<Props> = ({ Component, idName }) => {
-  const Hoc: React.FC = () => (
-    <motion.div
-    //   variants={staggerContainer()}
-    //   initial="hidden"
-    //   whileInView="show"
-    //   viewport={{ once: true, amount: 0.25 }}
-    //   className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-    >
-      <Component />
-    </motion.div>
-  );
-  return <Hoc />;
+	const Hoc: React.FC = () => (
+		<motion.div
+			variants={staggerContainer()}
+			initial="hidden"
+			whileInView="show"
+			viewport={{ once: true, amount: 0.25 }}
+			className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+		>
+			<Component />
+		</motion.div>
+	);
+	return <Hoc />;
 };
 
 export default SectionWrapper;
