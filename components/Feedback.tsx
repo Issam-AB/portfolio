4739f9114e13a3cb4c5eb7ac36aa41709faab9de
Issam <Issam.aboulfadl05@gmail.com>
2023-03-19 18:19:17 +0000

    finish work section

--- components/Feedback.tsx
+++ components/Feedback.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "./hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
 
 const Feedback: React.FC = () => {
-	return <div>Feedback</div>;
+	return (
+		<div className="mt-12 bg-black-100 rounded-[20px]">
+			<div
+				className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px] `}
+			>
+				<motion.div variants={textVariant()}>
+					<p className={styles.sectionSubText}>What others say</p>
+					<h2 className={styles.sectionHeadText}>Testimonials.</h2>
+				</motion.div>
+			</div>
+			<div className={`${styles}`}>
+
+			</div>
+		</div>
+	);
 };
 
-export default Feedback;
+export default () => <SectionWrapper Component={Feedback} idName="" />;
